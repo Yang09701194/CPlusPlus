811c14aaa9abd2eb9355788ce57fde0a0cc51ad4
Yang09701194 <mustnecessity@gmail.com>
2020-06-07 15:33:05 +0800

    p15

--- CppProject/CppProject/CppProject.cpp
+++ CppProject/CppProject/CppProject.cpp
@@ -9,12 +9,49 @@ using namespace std;
 
 int main() {
 	
-	cout << "Enter 2 numbers:" << endl;
-	int v1 = 0, v2 = 0;
-	cin >> v1 >> v2;
-	cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
+	int value = 0;
+	cout << "c" << endl;
+
+	while (std::cin >> value)
+	{
+		cout << "c" << endl;
+	}
+
 	return 0;
 }
+
+
+
+
+
+
+//p15
+//cin  一直讀到結尾或錯誤非數字    果然快  到這邊打這段程式的熟練度和打字速度  已經跟C#差不多了
+//
+//int value = 0;
+//cout << "c" << endl;
+//
+//while (std::cin >> value)    Ctrl Z 真的是 eof
+//{
+//	cout << "c" << endl;    這邊怪  不會進來  
+//}
+//
+//return 0;
+
+
+//p6
+//
+//
+//int main() {
+//
+//	cout << "Enter 2 numbers:" << endl;
+//	int v1 = 0, v2 = 0;
+//	cin >> v1 >> v2;
+//	cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
+//	return 0;
+//}
+//
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
